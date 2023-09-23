1f7ddf1448c94d73fdce303751b391a4e6b67fc0
devbossma <ysabere201@gmail.com>
2023-09-23 01:12:09 +0100

    adding shift operators.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java!!");
-         DataTypes.primitiveDataTypes();
-         DataTypes.nonPremitiveData();
-         OutputAndInputInJava.input();
-         OutputAndInputInJava.input();
-         OperatorsInJava.assignmentOperator();
-         OperatorsInJava.arithmeticOperator();
-         OperatorsInJava.unaryOperator();
-         OperatorsInJava.logicalOperator();
+        DataTypes.primitiveDataTypes();
+        DataTypes.nonPremitiveData();
+        OutputAndInputInJava.input();
+        OutputAndInputInJava.output();
+        OperatorsInJava.assignmentOperator();
+        OperatorsInJava.arithmeticOperator();
+        OperatorsInJava.unaryOperator();
+        OperatorsInJava.logicalOperator();
+        OperatorsInJava.shiftOperator();
+
     }
 
 }
--- src/OperatorsInJava.java
+++ src/OperatorsInJava.java
@@ -6,7 +6,6 @@ public class OperatorsInJava {
 
     /*
     * Types of Java Operators:
-    * Resource link: https://www.mygreatlearning.com/blog/java-operators/
     * 1. Unary Operators
     * 2. Arithmetic Operators
     * 3. Assignment Operators
@@ -134,6 +133,84 @@ public class OperatorsInJava {
         System.out.println(!b);
         System.out.println ( ! ( a  &&  b ));
         System.out.println ( ! ( a  ||  b ));
+    }
+    public static void shiftOperator(){
+        /*
+        * Description:
+        * A shift operator performs bit manipulation on data by shifting the bits of its first operand right or left.
+        * */
+        int i = 24;
+
+        // Extra:
+        // The binary format of this integer (i) is  ( 11000 ),
+        // you can convert any integer to binary format by using this method:  Integer.toBinaryString(int integer_x);
+        // and if you want to get the int value of a binary, you can use the methode: Integer.parseInt(String binary_string);
+        String binary = Integer.toBinaryString(i);
+        System.out.println("********** Before applying shift operator  *************");
+        System.out.printf("***************************\n|\tint: %s bits: %s   |\n***************************\n", i, binary);
+        System.out.println(binary);
+
+
+
+
+
+        /****************************************** ( >> ) Right Shift operator ***************************************************/
+
+        // This operator right shifts the bits of the left_operand,
+        // the right_operand decides the number of places to shift.
+        // Syntax: left_operand  >>  right_operand
+        i = i >> 2; // 11000 => (00110 or 110)
+        binary = Integer.toBinaryString(i);
+
+        System.out.println("********** after Riht shift by 2 *************");
+        System.out.printf("***************************\n|\tint: %s bits: %s   |\n***************************\n", i, binary);
+
+
+
+
+
+
+
+
+        /****************************************** ( << ) Left Shift operator ***************************************************/
+
+        // This operator left shifts the bits of the left_operand,
+        // the right_operand decides the number of places to shift.
+        // Syntax: left_operand  <<  right_operand
+        i = 24; // 11000
+        i = i << 2; // 11000 => 1100000
+        binary = Integer.toBinaryString(i);
+        System.out.println("********** after Left shift by 2 *************");
+        System.out.printf("***************************\n|\tint: %s bits: %s |\n***************************\n", i, binary);
+
+        /*
+        * if you still confused about this operators
+        * think like so:
+        * the right_operand is a number of places (bits)
+        * you can add those places to your binary string, or you can remove them from.
+        * NB: the removing or adding those places are always be applying to the end of the binary string.
+        * with the left_shift (<<) we add those places as a zeros to the end.
+        * with the right_shift (>>) we remove those number of bits from the end also.
+        *
+        * Ex: 20 <=> '10100'.
+        *   I: so if the syntax is like 20 << 3.
+        *       we have two (informations)
+        *       1: right_operand = 3 so 3 places
+        *       2: (<<) is left_shift operator,  we should ((Add)) 3 places as a zeros to the end.
+        *       result:  '10100' + '000' so the whole string wil be like '10100000'
+        *       and if we convert this string to int value we will get 160.
+        *   II: like so, if the syntax is like 20 >> 3.
+        *       we have two (informations) again.
+        *       1: right_operand = 3 so 3 places. is the same like above.
+        *       2: (>>) is right_shift operator, so we  should ((remove)) 3 places as a bits from the end.
+        *       result:  '10(100)' - '100' so the new string wil be like '10'.
+        *       and if we convert this string to int value we will get 2.
+        * *********** now go back to the code above and try to replace the variable (i) by 20 and the right operend
+        * *********** by 3. to get those theoretical results.
+        * */
+
+
+
     }
 
 

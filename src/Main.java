a306839bf8cdb0a36f23ebe160d2a9c0a2d363dd
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-09-24 02:13:40 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello java!!");
-        DataTypes.primitiveDataTypes();
-        DataTypes.nonPremitiveData();
-        OutputAndInputInJava.input();
-        OutputAndInputInJava.output();
-        OperatorsInJava.assignmentOperator();
-        OperatorsInJava.arithmeticOperator();
-        OperatorsInJava.unaryOperator();
-        OperatorsInJava.logicalOperator();
-        OperatorsInJava.shiftOperator();
-        OperatorsInJava.bitwiseOperator();
+        //System.out.println("Hello java!!");
+        //DataTypes.primitiveDataTypes();
+        //DataTypes.nonPremitiveData();
+        //OutputAndInputInJava.input();
+        //OutputAndInputInJava.output();
+        //OperatorsInJava.assignmentOperator();
+        //OperatorsInJava.arithmeticOperator();
+        //OperatorsInJava.unaryOperator();
+        //OperatorsInJava.logicalOperator();
+        //OperatorsInJava.shiftOperator();
+        //OperatorsInJava.bitwiseOperator();
 
     }
 

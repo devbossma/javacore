aae10e8adf87ab4ca6c2ebaf625bbc34ff62293f
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-09-26 01:32:53 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        /*System.out.println("Hello java!!");
-        DataTypes.primitiveDataTypes();
-        DataTypes.nonPremitiveData();
-        OutputAndInputInJava.input();
-        OutputAndInputInJava.output();
-        OperatorsInJava.assignmentOperator();
-        OperatorsInJava.arithmeticOperator();
-        OperatorsInJava.unaryOperator();
-        OperatorsInJava.logicalOperator();
-        OperatorsInJava.shiftOperator();
-        OperatorsInJava.bitwiseOperator();
-        OperatorsInJava.ternaryOperator();
-        OperatorsInJava.relationalOperator();
-*/
-        StringManipulation.stringConcatenation();
-        StringManipulation.stringComparison();
+        //System.out.println("Hello java!!");
+        
+        //DataTypes.primitiveDataTypes();
+        //DataTypes.nonPremitiveData();
+        
+        //OutputAndInputInJava.input();
+        //OutputAndInputInJava.output();
+        
+        //OperatorsInJava.assignmentOperator();
+        //OperatorsInJava.arithmeticOperator();
+        //OperatorsInJava.unaryOperator();
+        //OperatorsInJava.logicalOperator();
+        //OperatorsInJava.shiftOperator();
+        //OperatorsInJava.bitwiseOperator();
+        //OperatorsInJava.ternaryOperator();
+        //OperatorsInJava.relationalOperator();
+
+        //StringManipulation.stringConcatenation();
+        //StringManipulation.stringComparison();
     }
 
 }

5ba2a74a1dcc242e41f37325253206ba7e62d43f
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-11 00:04:57 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -33,8 +33,8 @@ public class Main {
 
         //ifStatement.ifStatement();
         //ifStatement.ifThenElse();
-        ifStatement.ifThenElseif();
-        whileLoop.whileStatement();
+        //ifStatement.ifThenElseif();
+        //whileLoop.whileStatement();
 
 
 

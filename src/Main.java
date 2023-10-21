49b5fde8c9306f4b4bc28ccc288b2e55d034fa40
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-10-21 22:01:07 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import ControlFlowStatements.ifStatement;
+yoimport ControlFlowStatements.ifStatement;
 import ControlFlowStatements.whileLoop;
 import datastructure.StackDataStructure;
 
@@ -37,7 +37,7 @@ public class Main {
         //ifStatement.ifThenElseif();
         //whileLoop.whileStatement();
 
-        //StackDataStructure.stackImplementation();
+        StackDataStructure.stackImplementation();
 
 
 

e4b80305f6d784b6bf8e4ce58d50c89841513ef0
S.Yassine <80557645+devbossma@users.noreply.github.com>
2023-11-03 21:58:10 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -37,7 +37,7 @@ public class Main {
         //ifStatement.ifThenElseif();
         //whileLoop.whileStatement();
 
-        StackDataStructure.stackImplementation();
+        //StackDataStructure.stackImplementation();
 
 
 

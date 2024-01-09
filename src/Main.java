c0e75f942c26d19a52c7f9387f8dd11bdae22a36
S.Yassine <80557645+devbossma@users.noreply.github.com>
2024-01-09 21:38:48 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import datastructure.StackDataStructure;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello java!!");
+        //System.out.println("Hello java!!");
         
         //DataTypes.primitiveDataTypes();
         //DataTypes.nonPremitiveData();
@@ -37,7 +37,7 @@ public class Main {
         //ifStatement.ifThenElseif();
         //whileLoop.whileStatement();
 
-        //StackDataStructure.stackImplementation();
+        StackDataStructure.stackImplementation();
 
 
 
